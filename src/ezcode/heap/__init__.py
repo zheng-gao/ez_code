f33b967e2cef1a1e1adbbcc001cd05c5cf51a29e
Zheng Gao <zgao@linkedin.com>
2022-02-23 18:42:11 -0800

    Update PriorityQueue

--- src/ezcode/heap/__init__.py
+++ src/ezcode/heap/__init__.py
@@ -1,14 +1,49 @@
 import heapq
 
 
+# class PriorityQueue:
+#     def __init__(self, data: list = None, min_heap: bool = True):
+#         self.min_heap = min_heap
+#         self.heap = data.copy() if data else list()  # (priority, key)
+#         if self.min_heap:
+#             heapq.heapify(self.heap)
+#         else:
+#             heapq._heapify_max(self.heap)
+# 
+#     def __len__(self):
+#         return len(self.heap)
+# 
+#     def __str__(self):
+#         return str(self.heap)
+# 
+#     def peek(self):
+#         """ O(1) """
+#         if len(self) <= 0:
+#             raise IndexError("Peek at an empty queue")
+#         return self.heap[0]
+# 
+#     def push(self, data):
+#         """ O(logN) """
+#         if self.min_heap:
+#             heapq.heappush(self.heap, data)
+#         else:
+#             self.heap.append(data)
+#             heapq._siftdown_max(self.heap, 0, len(self.heap) - 1)  # python siftdown is from leaf to root
+# 
+#     def pop(self):
+#         """ O(logN) """
+#         if len(self) <= 0:
+#             raise IndexError("Pop on an empty queue")
+#         return heapq.heappop(self.heap) if self.min_heap else heapq._heappop_max(self.heap)
+
+
 class PriorityQueue:
-    def __init__(self, data: list = None, min_heap: bool = True):
+    def __init__(self, init_queue: list = None, min_heap: bool = True):
         self.min_heap = min_heap
-        self.heap = data.copy() if data else list()  # (priority, key)
-        if self.min_heap:
-            heapq.heapify(self.heap)
-        else:
-            heapq._heapify_max(self.heap)
+        self.heap = list()  # (priority, key)
+        if init_queue:
+            for item in init_queue:
+                self.push(item)
 
     def __len__(self):
         return len(self.heap)
@@ -20,46 +55,75 @@ class PriorityQueue:
         """ O(1) """
         if len(self) <= 0:
             raise IndexError("Peek at an empty queue")
-        return self.heap[0]
+        return self.heap[0][0] if len(self.heap[0]) == 1 else self.heap[0]  # priority / priority, key
 
-    def push(self, data):
+    def push(self, *priority_n_key):
         """ O(logN) """
-        if self.min_heap:
-            heapq.heappush(self.heap, data)
+        if len(priority_n_key) == 0:
+            raise KeyError("Nothing to push")
+        elif len(priority_n_key) == 1:
+            self.heap.append(priority_n_key[0] if type(priority_n_key[0]) is tuple else (priority_n_key[0],))
         else:
-            self.heap.append(data)
-            heapq._siftdown_max(self.heap, 0, len(self.heap) - 1)  # python siftdown is from leaf to root
+            self.heap.append((priority_n_key[0], priority_n_key[1]))
+        self._sift_down(len(self.heap) - 1)
 
     def pop(self):
         """ O(logN) """
         if len(self) <= 0:
-            raise IndexError("Pop on an empty queue")
-        return heapq.heappop(self.heap) if self.min_heap else heapq._heappop_max(self.heap)
+            raise IndexError("Pop from an empty queue")
+        else:
+            top_item = self.heap[0]
+            self.heap[0] = self.heap[-1]
+            self.heap.pop()
+            if len(self) > 0:
+                self._sift_up(0)
+            return top_item[0] if len(top_item) == 1 else top_item  # priority / priority, key
+
+    def _sift_down(self, index: int):
+        """ python sift down is from leaf to root, O(logN) """
+        new_item = self.heap[index]
+        while index > 0:
+            parent_index = (index - 1) >> 1
+            parent = self.heap[parent_index]
+            if (self.min_heap and new_item[0] < parent[0]) or (not self.min_heap and new_item[0] > parent[0]):
+                self.heap[index] = parent
+                index = parent_index
+            else:
+                break
+        self.heap[index] = new_item
+
+    def _sift_up(self, index: int):
+        """ python sift up is from root to leaf, O(logN) """
+        end_index = len(self.heap) - 1
+        new_item = self.heap[index]
+        left_index = (index << 1) + 1
+        while left_index <= end_index:
+            right_index = left_index + 1
+            child_index = left_index
+            if right_index <= end_index:
+                left_child, right_child = self.heap[left_index], self.heap[right_index]
+                if (self.min_heap and right_child[0] < left_child[0]) or (not self.min_heap and right_child[0] > left_child[0]):
+                    child_index = right_index
+            child = self.heap[child_index]
+            if (self.min_heap and child[0] < new_item[0]) or (not self.min_heap and child[0] > new_item[0]):
+                self.heap[index] = child
+                index = child_index
+                left_index = (index << 1) + 1
+            else:
+                break
+        self.heap[index] = new_item
 
 
-class PriorityMap:
-    def __init__(self, map: dict = None, min_heap: bool = True):
-        self.min_heap = min_heap
-        self.heap = list() # (priority, key)
+class PriorityMap(PriorityQueue):
+    def __init__(self, init_map: dict = None, min_heap: bool = True):
+        super().__init__(min_heap=min_heap)
         self.map = dict() # <key, heap_index>
-        if map:
-            for key, priority in map.items():
+        if init_map:
+            for key, priority in init_map.items():
                 self.push(priority, key)
-
-    def __len__(self):
-        return len(self.heap)
-
-    def __str__(self):
-        return str(self.heap)
 
     def __contains__(self, key):
         return key in self.map
-
-    def peek(self):
-        """ O(1) """
-        if len(self) <= 0:
-            raise IndexError("Peek at an empty queue")
-        return self.heap[0]
 
     def get_priority(self, key):
         """ O(1) """
@@ -67,9 +131,15 @@ class PriorityMap:
             raise KeyError(f"{key} not found")
         return self.heap[self.map[key]][0]
 
-    def push(self, *priority_key):
+    def peek(self):
+        """ O(1) """
+        if len(self) <= 0:
+            raise IndexError("Peek at an empty queue")
+        return self.heap[0]
+
+    def push(self, *priority_n_key):
         """ O(logN) """
-        priority, key = priority_key[0] if type(priority_key[0]) is tuple else (priority_key[0], priority_key[1])
+        priority, key = priority_n_key[0] if type(priority_n_key[0]) is tuple else (priority_n_key[0], priority_n_key[1])
         if key in self:
             self.update(priority, key)
         else:
@@ -82,9 +152,6 @@ class PriorityMap:
         """ O(logN) """
         if len(self) <= 0:
             raise IndexError("Pop from an empty queue")
-        elif len(self) == 1:
-            del self.map[self.heap[0][1]]
-            return self.heap.pop()
         else:
             top_item = self.heap[0]
             tail_item = self.heap[-1]
@@ -92,8 +159,9 @@ class PriorityMap:
             self.map[tail_item[1]] = 0
             self.heap.pop()
             del self.map[top_item[1]]
-            self._sift_up(0)
-            return top_item
+            if len(self) > 0:
+                self._sift_up(0)
+            return top_item  # priority, key
 
     def delete(self, key):
         """ O(logN) """
@@ -107,9 +175,9 @@ class PriorityMap:
         self.heap.pop()
         self._sift_up(index)
 
-    def update(self, *priority_key):
+    def update(self, *priority_n_key):
         """ O(logN) """
-        priority, key = priority_key[0] if type(priority_key[0]) is tuple else (priority_key[0], priority_key[1])
+        priority, key = priority_n_key[0] if type(priority_n_key[0]) is tuple else (priority_n_key[0], priority_n_key[1])
         if key not in self:
             raise KeyError(f"{key} not found")
         index = self.map[key]
--- tests/test_heap.py
+++ tests/test_heap.py
@@ -2,31 +2,38 @@ from ezcode.heap import PriorityQueue, PriorityMap
 
 
 def test_priority_queue():
+    min_queue = PriorityQueue([4, 3, 5, 1, 2])
+    for pop_data in [1, 2, 3, 4, 5]:
+        assert min_queue.pop() == pop_data
+    max_queue = PriorityQueue([4, 3, 5, 1, 2], min_heap=False)
+    for pop_data in [5, 4, 3, 2, 1]:
+        assert max_queue.pop() == pop_data
+
     push_list = [(4, "D"), (3, "C"), (5, "E"), (1, "A"), (2, "B")]
 
-    min_pq = PriorityQueue()
+    min_queue = PriorityQueue()
     min_peek_list = [(4, "D"), (3, "C"), (3, "C"), (1, "A"), (1, "A")]
     for push_data, peek_data in zip(push_list, min_peek_list):
-        min_pq.push(push_data)
-        assert min_pq.peek() == peek_data
+        min_queue.push(push_data)
+        assert min_queue.peek() == peek_data
     min_pop_list = [(1, "A"), (2, "B"), (3, "C"), (4, "D"), (5, "E")]
     for pop_data in min_pop_list:
-        assert min_pq.pop() == pop_data
-    min_pq = PriorityQueue(push_list)
+        assert min_queue.pop() == pop_data
+    min_queue = PriorityQueue(push_list)
     for pop_data in min_pop_list:
-        assert min_pq.pop() == pop_data
+        assert min_queue.pop() == pop_data
 
-    max_pq = PriorityQueue(min_heap=False)
+    max_queue = PriorityQueue(min_heap=False)
     max_peek_list = [(4, "D"), (4, "D"), (5, "E"), (5, "E"), (5, "E")]
     for push_data, peek_data in zip(push_list, max_peek_list):
-        max_pq.push(push_data)
-        assert max_pq.peek() == peek_data
+        max_queue.push(push_data)
+        assert max_queue.peek() == peek_data
     max_pop_list = [(5, "E"), (4, "D"), (3, "C"), (2, "B"), (1, "A")]
     for pop_data in max_pop_list:
-        assert max_pq.pop() == pop_data
-    max_pq = PriorityQueue(push_list, min_heap=False)
+        assert max_queue.pop() == pop_data
+    max_queue = PriorityQueue(push_list, min_heap=False)
     for pop_data in max_pop_list:
-        assert max_pq.pop() == pop_data
+        assert max_queue.pop() == pop_data
 
 
 def test_priority_map():
@@ -78,3 +85,69 @@ def test_priority_map():
     max_pop_list = [(5, "E"), (3, "C"), (1, "A")]
     for pop_data in max_pop_list:
         assert max_map.pop() == pop_data
+
+
+def test_heap_custom_comparator():
+
+    class Priority:
+        def __init__(self, a, b, c):
+            self.a = a
+            self.b = b
+            self.c = c
+
+        def check_type(self, other):
+            if not isinstance(other, type(self)): 
+                raise NotImplemented(f"{other} is not type {type(self)}")
+
+        def __str__(self):
+            return f"({self.a},{self.b},{self.c})"
+
+        def __eq__(self, other) -> bool:
+            self.check_type(other)
+            return not self < other and not other < self
+
+        def __gt__(self, other) -> bool:
+            self.check_type(other)
+            return other < self
+
+        def __lt__(self, other) -> bool:
+            self.check_type(other)
+            if self.a < other.a:
+                return True
+            if other.a < self.a:
+                return False
+            if self.b < other.b:
+                return True
+            if other.b < self.b:
+                return False
+            return self.c < self.c
+
+    push_list = [(Priority(2,2,3), "A"), (Priority(2,1,1), "B"), (Priority(1,1,1), "E"), (Priority(1,1,2), "D"), (Priority(1,1,1), "C")]
+
+    min_queue = PriorityQueue(push_list)
+    push_map = {"A": Priority(2,2,3), "B": Priority(2,1,1), "E": Priority(1,1,1), "D": Priority(1,1,2), "C": Priority(1,1,1)}
+    min_map = PriorityMap(push_map)
+    min_pop_list = [(Priority(1,1,1), "E"), (Priority(1,1,1), "C"), (Priority(1,1,2), "D"), (Priority(2,1,1), "B"), (Priority(2,2,3), "A")]
+    for pop_data in min_pop_list:
+        assert min_queue.pop() == pop_data
+        assert min_map.pop() == pop_data
+
+    max_queue = PriorityQueue(push_list, min_heap=False)
+    max_pop_list = [(Priority(2,2,3), "A"), (Priority(2,1,1), "B"), (Priority(1,1,2), "D"), (Priority(1,1,1), "E"), (Priority(1,1,1), "C")]
+    max_map = PriorityMap(min_heap=False)
+    for push_data in push_list:
+        max_map.push(push_data)
+    for pop_data in max_pop_list:
+        assert max_queue.pop() == pop_data
+        assert max_map.pop() == pop_data
+
+
+
+
+
+
+
+
+
+
+
