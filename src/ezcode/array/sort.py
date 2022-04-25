5d6008dea89be1a58c4e7ef762ba4699b7499fd6
Zheng Gao <zgao@linkedin.com>
2022-04-24 18:41:56 -0700

    Add reverse to quick_sort

--- src/ezcode/array/sort.py
+++ src/ezcode/array/sort.py
@@ -2,38 +2,38 @@ import random
 from ezcode.array.utils import swap
 
 
-def partition_with_first_item_as_pivot(array: list, begin: int, end: int):
+def partition_with_first_item_as_pivot(array: list, begin: int, end: int, reverse: bool = False):
     pivot, b = begin, begin + 1  # b point to the item larger than or equals to array[pivot].
     for e in range(begin + 1, end + 1):
-        if array[e] <= array[pivot]:
+        if (not reverse and array[e] <= array[pivot]) or (reverse and array[pivot] <= array[e]):
             swap(array, b, e)
             b += 1
     swap(array, b - 1, pivot)
     return b - 1
 
 
-def partition(array: list, begin: int, end: int, pivot: int):
+def partition(array: list, begin: int, end: int, pivot: int, reverse: bool = False):
     if pivot < begin or pivot > end:
         raise ValueError(f"pivot {pivot} out of range [{begin}, {end}]")
     swap(array, begin, pivot)
-    return partition_with_first_item_as_pivot(array, begin, end)
+    return partition_with_first_item_as_pivot(array, begin, end, reverse)
 
 
-def partition_with_random_pivot(array: list, begin: int, end: int):
+def partition_with_random_pivot(array: list, begin: int, end: int, reverse: bool = False):
     pivot = random.randint(begin, end)  # [begin, end]
-    return partition(array, begin, end, pivot)
+    return partition(array, begin, end, pivot, reverse)
 
 
-def quick_sort(array: list, begin: int = None, end: int = None):
+def quick_sort(array: list, begin: int = None, end: int = None, reverse: bool = False):
     # inplace sort array[begin:end]
     if begin is None:
         begin = 0
     if end is None:
         end = len(array) - 1
     if(begin < end):
-        pivot_index = partition_with_random_pivot(array, begin, end)
-        quick_sort(array, begin, pivot_index - 1)
-        quick_sort(array, pivot_index + 1, end)
+        pivot_index = partition_with_random_pivot(array, begin, end, reverse)
+        quick_sort(array, begin, pivot_index - 1, reverse)
+        quick_sort(array, pivot_index + 1, end, reverse)
 
 
 
--- tests/test_array.py
+++ tests/test_array.py
@@ -192,9 +192,12 @@ def test_split_chunk_list():
 
 
 def test_quick_sort():
-    data  = [7, 2, 4, 6, 5, 4, 1, 3, 8, 0, 6, 9, 4]
-    quick_sort(data)
-    assert data == [0, 1, 2, 3, 4, 4, 4, 5, 6, 6, 7, 8, 9]
+    data_1 = [7, 2, 4, 6, 5, 4, 1, 3, 8, 0, 6, 9, 4]
+    data_2 = data_1.copy()
+    quick_sort(data_1, reverse=False)
+    quick_sort(data_2, reverse=True)
+    assert data_1 == [0, 1, 2, 3, 4, 4, 4, 5, 6, 6, 7, 8, 9]
+    assert data_2 == [9, 8, 7, 6, 6, 5, 4, 4, 4, 3, 2, 1, 0]
 
 
 
