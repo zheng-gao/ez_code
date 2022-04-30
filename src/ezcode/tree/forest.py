d2e19d5120d00186deb51a9d5a12b25c9115f325
Zheng Gao <zgao@linkedin.com>
2022-04-30 04:09:00 -0700

    Add DisjointSets

--- src/ezcode/tree/forest.py
+++ src/ezcode/tree/forest.py
@@ -1,7 +1,7 @@
 class DisjointSets:
     def __init__(self, nodes: set):
         self.parents = dict()
-        self.tree_ranks = dict()  # Do not use tree heights, calculate height is O(N), disjon method will break the ranks
+        self.tree_ranks = dict()  # Do not use tree heights, calculate height is O(N)
         self.tree_sizes = dict()
         for node in nodes:
             self.parents[node] = node
@@ -13,22 +13,31 @@ class DisjointSets:
         return self.number_of_disjoint_sets
 
     def union(self, node1, node2):
+        """
+            return: True merged, False already in the same set
+        """
         parent1, parent2 = self.find(node1), self.find(node2)
         if parent1 != parent2:
             if self.tree_ranks[parent1] < self.tree_ranks[parent2]:
                 parent1, parent2 = parent2, parent1
             if self.tree_ranks[parent1] == self.tree_ranks[parent2]:
                 self.tree_ranks[parent1] += 1
-            # if self.tree_sizes[parent1] < self.tree_sizes[parent2]:
-            #     parent1, parent2 = parent2, parent1
             self.parents[parent2] = parent1
             self.tree_sizes[parent1] += self.tree_sizes[parent2]
             self.number_of_disjoint_sets -= 1
+            return True
+        return False
 
     def find(self, node):
-        """ O(logN) on average """
+        """ O(logN) on average (amortized) """
         if node in self.parents:
-            return node if self.parents[node] == node else self.find(self.parents[node])
+            path_compression = list()
+            while node != self.parents[node]:
+                node = self.parents[node]
+                path_compression.append(node)
+            for n in path_compression:
+                self.parents[n] = node
+            return node
         raise ValueError(f"Node \"{node}\" not found in disjoint sets")
 
     def is_joint(self, node1, node2):
@@ -36,15 +45,3 @@ class DisjointSets:
 
     def get_set_size(self, node):
         return self.tree_sizes[self.find(node)]
-
-    # def disjoin(self, node):
-    #     parent = self.find(node)
-    #     if parent != node:
-    #         self.parents[node] = node
-    #         self.tree_sizes[parent] -= self.tree_sizes[node]
-    #         self.number_of_disjoint_sets += 1
-
-
-
-
-
