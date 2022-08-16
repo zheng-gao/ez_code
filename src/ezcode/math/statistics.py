699038622e8e115d0f200995a5aa9400b1947156
Zheng Gao <zgao@linkedin.com>
2022-08-16 02:26:11 -0700

    Fix RandomGenerator

--- src/ezcode/math/statistics.py
+++ src/ezcode/math/statistics.py
@@ -3,18 +3,18 @@ import random
 
 class RandomGenerator:
     def __init__(self, weights: list):
-        self.sum = 0
-        self.stairs = list()
+        self.sum,  = 0
+         = list()
         for weight in weights:
             self.sum += weight
-            self.stairs.append(self.sum)
+            self.cumulative_sum.append(self.sum)
 
     def random_index(self) -> int:
         r = random.random() * self.sum
-        begin, end = 0, len(self.stairs) - 1
+        begin, end = 0, len(self.cumulative_sum) - 1
         while begin < end:
             mid = begin + (end - begin) // 2
-            if r < self.stairs[mid]:
+            if r < self.cumulative_sum[mid]:
                 end = mid
             else:
                 begin = mid + 1
