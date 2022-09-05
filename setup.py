b00eea7baac4498b4e9a61ebff0af5fe40e7119e
Zheng Gao <zgao@linkedin.com>
2022-09-05 04:04:37 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as file_handler:
 
 setuptools.setup(
     name="ezcode",
-    version="0.0.95",
+    version="0.0.96",
     author="Zheng Gao",
     author_email="mail.zheng.gao@gmail.com",
     description="Easy Algorithm & Data Structure",
