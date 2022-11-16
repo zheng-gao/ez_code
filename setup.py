5de969710d30421115d6c9508b684db40bc19b5e
Zheng Gao <zgao@linkedin.com>
2022-11-16 13:21:28 -0800

    Fix readme

--- setup.py
+++ setup.py
@@ -3,25 +3,24 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 
-def parse_readme(readme_path):
-    """
-    readme.md format:
-    # Content
-    1. [Subject1](path1.md)
-    2. [Subject2](path2.md)
-    ...
-    """
-    with open(file=readme_path, mode="r", encoding="utf-8") as f:
-        lines = f.readlines()
-    topic = lines[0].rstrip()
-    https://pypi.org/project/ezcode/#q1-whats-the-max-number-of-items-can-you-put-into-the-knapsack
+# def parse_readme(readme_path):
+#     """
+#     readme.md format:
+#     # Content
+#     1. [Subject1](path1.md)
+#     2. [Subject2](path2.md)
+#     ...
+#     """
+#     with open(file=readme_path, mode="r", encoding="utf-8") as f:
+#         lines = f.readlines()
+#     topic = lines[0].rstrip()
 
 
 def build_long_description():
     long_description=""
     docs = Path(__file__).parent / "docs"
     for md in sorted(docs.glob("*.md")):
-        with open(file=md, mode="r", encoding="utf-8") as f:
+        with open(file=readme, mode="r", encoding="utf-8") as f:
             long_description += f.read()
     return long_description
 
@@ -34,7 +33,8 @@ setup(
     description="Easy Algorithm & Data Structure",
     url="https://github.com/zheng-gao/ez_code",
     project_urls={"Bug Tracker": "https://github.com/zheng-gao/ez_code/issues"},
-    long_description=build_long_description(),
+    # long_description=build_long_description(),
+    long_description=open(file=Path(__file__).parent/"readme.md", mode="r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
