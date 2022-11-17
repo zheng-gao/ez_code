e2b30408839ce8a2f04982ad158738a75e220655
Zheng Gao <zgao@linkedin.com>
2022-11-17 03:53:10 -0800

    Add knuth_shuffle

--- src/ezcode/random/__init__.py
+++ src/ezcode/random/__init__.py
@@ -1,5 +1,13 @@
 from collections import defaultdict
-from random import choice
+from random import choice, randint
+from ezcode.array.utils import swap
+
+
+def knuth_shuffle(array: list):
+    # https://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+    for i in range(len(array) - 1, 0, -1):
+        j = randint(0, i)  # 0 <= j <=i
+        swap(array, i, j)
 
 
 class RandomMultiSet:
