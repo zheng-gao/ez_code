413d257b653d42846b41d0761c003f1efe9ba1a4
Zheng Gao <zgao@linkedin.com>
2022-11-17 03:54:09 -0800

    Bump version 0.1.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ezcode",
-    version="0.1.7",
+    version="0.1.8",
     author="Zheng Gao",
     author_email="mail.zheng.gao@gmail.com",
     description="Easy Algorithm & Data Structure",
