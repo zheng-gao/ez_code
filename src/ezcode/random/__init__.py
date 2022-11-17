039e18a5b1573b1caf1ceab5096ec19a5be8b31c
Zheng Gao <zgao@linkedin.com>
2022-11-17 03:33:42 -0800

    Add RandomDictionary

--- src/ezcode/random/__init__.py
+++ src/ezcode/random/__init__.py
@@ -5,27 +5,28 @@ from random import choice
 class RandomMultiSet:
     def __init__(self):
         self.items = list()
-        self.item_indices = defaultdict(set)
+        self.indices = defaultdict(set)  # {item: {index1, index2, ...}}
 
     def __contains__(self, item) -> bool:
-        return item in self.item_indices
+        """ O(1) """
+        return item in self.indices
 
     def add(self, item):
         """ O(1) """
-        self.item_indices[item].add(len(self.items))
+        self.indices[item].add(len(self.items))
         self.items.append(item)
 
     def remove(self, item):
         """ O(1) """
-        if item in self.item_indices:
-            index = self.item_indices[item].pop()
-            if len(self.item_indices[item]) == 0:
-                del self.item_indices[item]
+        if item in self.indices:
+            index = self.indices[item].pop()
+            if len(self.indices[item]) == 0:
+                del self.indices[item]
             if index != len(self.items) - 1:
                 last_index, last_item = len(self.items) - 1, self.items[-1]
                 self.items[index] = last_item
-                self.item_indices[last_item].remove(last_index)
-                self.item_indices[last_item].add(index)
+                self.indices[last_item].remove(last_index)
+                self.indices[last_item].add(index)
             self.items.pop()
         else:
             raise KeyError(f"{item} not found")
@@ -33,3 +34,51 @@ class RandomMultiSet:
     def random(self):
         """ O(1) """
         return choice(self.items)
+
+
+class RandomDictionary:
+    def __init__(self):
+        self.keys = list()  # [key]
+        self.key_index_dict = dict()  # {key: key_index}
+        self.key_value_dict = dict()  # {key: value}
+
+    def __contains__(self, key) -> bool:
+        """ O(1) """
+        return key in self.key_index_dict
+
+    def __getitem__(self, key):
+        """ O(1) """
+        if key not in self.key_index_dict:
+            raise KeyError(f"{key} not found")
+        return self.key_value_dict[key]
+
+    def __setitem__(self, key, value):
+        """ O(1) """
+        if key not in self.key_index_dict:
+            self.key_index_dict[key] = len(self.keys)
+            self.keys.append(key)
+        self.key_value_dict[key] = value
+
+    def __delitem__(self, key):
+        """ O(1) """
+        if key not in self.key_index_dict:
+            raise KeyError(f"{key} not found")
+        index, last_key = self.key_index_dict[key], self.keys[-1]
+        self.keys[index] = last_key
+        self.key_index_dict[last_key] = index
+        self.keys.pop()
+        del self.key_index_dict[key]
+        del self.key_value_dict[key]
+
+    def random_key(self):
+        """ O(1), probability = 1 / total number of keys """
+        return choice(self.keys)
+
+    def random_value(self):
+        """ O(1), probability = value occurences / total number of values """
+        return self.key_value_dict[self.random_key()]
+
+
+
+
+
--- tests/test_random.py
+++ tests/test_random.py
@@ -1,4 +1,4 @@
-from ezcode.random import RandomMultiSet
+from ezcode.random import RandomMultiSet, RandomDictionary
 
 
 def test_random_multi_set():
@@ -7,19 +7,19 @@ def test_random_multi_set():
     rm_set.add("a")
     rm_set.add("b")
     rm_set.add("a")
-    assert rm_set.item_indices == {"a": {0, 1, 3}, "b": {2}}
+    assert rm_set.indices == {"a": {0, 1, 3}, "b": {2}}
     assert rm_set.items == ["a", "a", "b", "a"]
     rm_set.remove("b")
-    assert rm_set.item_indices == {"a": {0, 1, 2}}
+    assert rm_set.indices == {"a": {0, 1, 2}}
     assert rm_set.items == ["a", "a", "a"]
     rm_set.remove("a")
-    assert rm_set.item_indices == {"a": {0, 1}}
+    assert rm_set.indices == {"a": {0, 1}}
     assert rm_set.items == ["a", "a"]
     rm_set.remove("a")
-    assert rm_set.item_indices == {"a": {0}}
+    assert rm_set.indices == {"a": {0}}
     assert rm_set.items == ["a"]
     rm_set.remove("a")
-    assert rm_set.item_indices == {}
+    assert rm_set.indices == {}
     assert rm_set.items == []
     rm_set.add("a")
     rm_set.add("a")
@@ -27,13 +27,40 @@ def test_random_multi_set():
     rm_set.add("b")
     rm_set.add("c")
     rm_set.add("c")
-    assert rm_set.item_indices == {"a": {0, 1}, "b": {2, 3}, "c": {4, 5}}
+    assert rm_set.indices == {"a": {0, 1}, "b": {2, 3}, "c": {4, 5}}
     assert rm_set.items == ["a", "a", "b", "b", "c", "c"]
     rm_set.remove("a")
     rm_set.remove("a")
-    assert rm_set.item_indices == {"b": {2, 3}, "c": {0, 1}}
+    assert rm_set.indices == {"b": {2, 3}, "c": {0, 1}}
     assert rm_set.items == ["c", "c", "b", "b"]
     rm_set.remove("c")
     rm_set.remove("c")
-    assert rm_set.item_indices == {"b": {0, 1}}
+    assert rm_set.indices == {"b": {0, 1}}
     assert rm_set.items == ["b", "b"]
+
+
+def test_random_dictionary():
+    r_dict = RandomDictionary()
+    r_dict["a"] = "lower"
+    r_dict["*"] = "wildcard"
+    r_dict["!"] = "punctuation"
+    r_dict["b"] = "lower"
+    r_dict["B"] = "upper"
+    r_dict["?"] = "punctuation"
+    assert r_dict.keys == ['a', '*', '!', 'b', 'B', '?']
+    assert r_dict.key_index_dict == {'a': 0, '*': 1, '!': 2, 'b': 3, 'B': 4, '?': 5}
+    assert r_dict.key_value_dict == {'a': 'lower', '*': 'wildcard', '!': 'punctuation', 'b': 'lower', 'B': 'upper', '?': 'punctuation'}
+    del r_dict["B"]
+    assert r_dict.keys == ['a', '*', '!', 'b', '?']
+    assert r_dict.key_index_dict == {'a': 0, '*': 1, '!': 2, 'b': 3, '?': 4}
+    assert r_dict.key_value_dict == {'a': 'lower', '*': 'wildcard', '!': 'punctuation', 'b': 'lower', '?': 'punctuation'}
+    del r_dict["?"]
+    assert r_dict.keys == ['a', '*', '!', 'b']
+    assert r_dict.key_index_dict == {'a': 0, '!': 2, 'b': 3, '*': 1}
+    assert r_dict.key_value_dict == {'a': 'lower', '!': 'punctuation', 'b': 'lower', '*': 'wildcard'}
+    r_dict["A"] = "upper"
+    r_dict["c"] = "lower"
+    assert r_dict.keys == ['a', '*', '!', 'b', 'A', 'c']
+    assert r_dict.key_index_dict == {'a': 0, '!': 2, 'b': 3, '*': 1, 'A': 4, 'c': 5}
+    assert r_dict.key_value_dict == {'a': 'lower', '!': 'punctuation', 'b': 'lower', '*': 'wildcard', 'A': 'upper', 'c': 'lower'}
+
