dcd95a6b43ae71a5b8a3e5b1ae46f68537753374
Zheng Gao <zgao@linkedin.com>
2022-11-20 13:21:58 -0800

    Update long description

--- setup.py
+++ setup.py
@@ -2,9 +2,41 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 
+long_description = """
+# ezcode
+[![PyPi](https://img.shields.io/pypi/v/ezcode.svg)](https://pypi.python.org/pypi/ezcode)
+[![Github](https://img.shields.io/badge/github-master-brightgreen)](https://github.com/zheng-gao/ez_code)
+## Installation
+```bash
+pip3 install --upgrade ezcode
+```
+## Documentation
+* [docs](https://github.com/zheng-gao/ez_code/blob/main/docs)
+## Development Workflow
+```bash
+git clone https://github.com/zheng-gao/ez_code.git
+ez_code/ez.sh -d/--development
+```
+## Release Workflow
+```bash
+git clone https://github.com/zheng-gao/ez_code.git
+ez_code/ez.sh -r/--release
+```
+## Unit Test
+### Run all the tests
+```bash
+ez_code/ez.sh -o/--operations test
+```
+### Run a single test
+```bash
+ez_code/ez.sh -o/--operations test -a/--arguments test_array.py::test_binary_search
+```
+"""
+
+
 setup(
     name="ezcode",
-    version="0.1.12",
+    version="0.1.13",
     author="Zheng Gao",
     author_email="mail.zheng.gao@gmail.com",
     description="Easy Algorithm & Data Structure",
@@ -13,7 +45,7 @@ setup(
         "Documentation": "https://github.com/zheng-gao/ez_code/blob/main/docs/readme.md",
         "Bug Tracker": "https://github.com/zheng-gao/ez_code/issues",
     },
-    long_description=open(file=Path(__file__).parent/"readme.md", mode="r", encoding="utf-8").read(),
+    long_description=long_description,  # open(file=Path(__file__).parent/"readme.md", mode="r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
