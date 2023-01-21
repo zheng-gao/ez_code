ecb6de87fd521ded41ff1fce395049447fdd5855
Zheng Gao <zgao@linkedin.com>
2023-01-21 02:30:12 -0800

    Bump version

--- src/ezcode/Dynamic/SparseTable.py
+++ src/ezcode/Dynamic/SparseTable.py
@@ -75,6 +75,18 @@ class SparseTable:
                     if new_data != self.dp_table[row - row_offset][col + 1]:
                         self.dp_table[row - row_offset][col + 1] = new_data
                         _update_next(row - row_offset, col + 1)
+            """
+            [0,    1,    3,    7]                    [ 0,    1,    3,   10]
+            [1,    2,    4,    8]                    [ 1,    2,    4,   10]
+            [2,    3,    5,    9]                    [ 2,    3,    5,   10]
+            [3,    4,    6,    8]   merge = max      [ 3,    4,   10,   10]
+            [4,    5,    7,    7]  update(6, 10) =>  [ 4,    5,   10,   10]
+            [5,    6,    8,    8]                    [ 5,   10,   10,   10]
+            [6,    7,    9, -inf]                    [10,   10,   10, -inf]
+            [7,    8,    8, -inf]                    [ 7,    8,    8, -inf]
+            [8,    9, -inf, -inf]                    [ 8,    9, -inf, -inf]
+            [9, -inf, -inf, -inf]                    [ 9, -inf, -inf, -inf]
+            """
 
         validate_index(index, 0, len(self.dp_table) - 1)
         if self.dp_table[index][0] == data:
