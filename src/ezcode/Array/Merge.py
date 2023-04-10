1c2c47a30f0f5885ae75f5595fe3cfde1399e35c
Zheng Gao <zgao@linkedin.com>
2023-04-10 02:07:37 -0700

    Update merge_sorted_arrays

--- src/ezcode/Array/Merge.py
+++ src/ezcode/Array/Merge.py
@@ -1,7 +1,8 @@
+from typing import Callable
 from ezcode.Heap.PriorityQueue import PriorityQueue
 
 
-def merge_sorted_arrays(arrays: list[list], reverse=False):
+def merge_sorted_arrays(arrays: list[list], key: Callable = None, reverse=False):
     """
         Same as heapq.merge(*iterables, key=None, reverse=False)
         Time: O(NlogK) where N is all the elements in all arrays, K is the number of arrays
@@ -11,10 +12,12 @@ def merge_sorted_arrays(arrays: list[list], reverse=False):
     pq = PriorityQueue(min_heap=not reverse)
     for array_id, array in enumerate(arrays):
         if len(array) > 0:
-            pq.push((array_id, 0), array[0])
+            data = array[0] if key is None else key(array[0])
+            pq.push((array_id, 0), data)
     while len(pq) > 0:
-        item, priority = pq.pop(with_priority=True)
-        array_id, next_index = item[0], item[1] + 1
-        yield priority
-        if next_index < len(arrays[array_id]):
-            pq.push((array_id, next_index), arrays[array_id][next_index])
+        array_id, index = pq.pop()
+        yield arrays[array_id][index]
+        index += 1
+        if index < len(arrays[array_id]):
+            data = arrays[array_id][index] if key is None else key(arrays[array_id][index])
+            pq.push((array_id, index), data)
--- tests/Array/test_merge.py
+++ tests/Array/test_merge.py
@@ -13,3 +13,15 @@ def test_merge_sorted_arrays():
     assert list(merge_sorted_arrays(arrays)) == [
         1, 3, 7, 11, 12, 15, 16, 18, 25, 26, 28, 33, 40, 41, 42, 50, 57, 60
     ]
+
+    arrays = [
+        [(0, 3), (1, 4), (5, 6)],
+        [(1, 2), (3, 4)],
+        [(4, 10)]
+    ]
+    assert list(merge_sorted_arrays(arrays, key=lambda x: x[0])) == [
+        (0, 3), (1, 2), (1, 4), (3, 4), (4, 10), (5, 6)
+    ]
+    assert list(merge_sorted_arrays(arrays, key=lambda x: x[1])) == [
+        (1, 2), (0, 3), (3, 4), (1, 4), (5, 6), (4, 10)
+    ]
