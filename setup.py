8342b7ad9e39d81d0038b4e65cdac44721f3976d
Zheng Gao <zgao@linkedin.com>
2023-04-10 01:14:46 -0700

    bump verison

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ ez_code/ez.sh -o/--operations test -a/--arguments test_array.py::test_binary_sea
 
 setup(
     name="ezcode",
-    version="1.0.21",
+    version="1.0.22",
     author="Zheng Gao",
     author_email="mail.zheng.gao@gmail.com",
     description="Easy Algorithm & Data Structure",
